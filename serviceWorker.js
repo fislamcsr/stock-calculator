4a253502d42b982d19784a3da165ad7192d2ddce
Fakhrul Islam <fislamcsr@gmail.com>
2024-03-21 15:22:27 +0800

    Update serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -2,15 +2,15 @@
 var GHPATH = '/stock-calculator';
  
 // Choose a different app prefix name
-var APP_PREFIX = 'gppwa_';
+var APP_PREFIX = 'gppwasc_';
  
 // The version of the cache. Every time you change any of the files
 // you need to change this version (version_01, version_02…). 
 // If you don't change the version, the service worker will give your
 // users the old files!
-var VERSION = 'version_1';
+var VERSION = 'version_1.1';
 
-const stockCalculator = "stock-calculator-v1"
+const stockCalculator = "stock-calculator-v1.1"
 const assets = [
   `${GHPATH}/`,
   `${GHPATH}/index.html`,
